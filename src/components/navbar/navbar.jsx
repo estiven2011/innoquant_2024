7a8bab31a965f988bcdfa360243f3b692b9f4973
estiven2011 <juan_posada23221@elpoli.edu.co>
2024-04-02 11:53:29 -0500

    nav

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,40 +1,37 @@
-import React from 'react'
-import imgNavbar from '../../assets/pexels-energepiccom-110469.jpg'
-import "../navbar/navbar.css"
+import React from "react";
+import imgNavbar from "../../assets/pexels-energepiccom-110469.jpg";
+import "../navbar/navbar.css";
 import { CiUser } from "react-icons/ci";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-
-
-
-<div className=" fondo w-full flex  text-white font-semibold relative text-[1rem]"  >
-<div className=' flex w-full justify-between absolute mt-5 '>
-  <h1 className=' ml-2 text-[1.9rem]'>INNOCUAN</h1>
-  <ul className=' gap-10 flex justify-center '>
-    <NavLink to="/">
-    <li>Home</li>
-    </NavLink>
-    <NavLink>
-    <li>Nosotros</li>
-    </NavLink>
-    <NavLink to="/contacto">
-    <li>Contacto</li>
-    </NavLink>
-    <NavLink>
-    <li>Servicios</li>
-    </NavLink>
-  </ul>
-  <ul className=' gap-10 flex'>
-    <li className=' '><FaUser className=' text-[1.5rem] font-semibold' /></li>
-    <li></li>
-  </ul>
-  </div>
-</div>
-
+    <div className=" fondo w-full flex  text-white font-semibold relative text-[1rem]">
+      <div className=" flex w-full justify-between absolute mt-5 ">
+        <h1 className=" ml-4 mt-0 text-xl">INNOQUANT</h1>
+        <ul className=" gap-10 flex justify-center ">
+          <NavLink to="/">
+            <li>Home</li>
+          </NavLink>
+          <NavLink>
+            <li>Nosotros</li>
+          </NavLink>
+          <NavLink to="/contacto">
+            <li>Contacto</li>
+          </NavLink>
+          <NavLink>
+            <li>Servicios</li>
+          </NavLink>
+        </ul>
+        <ul className="gap-10 flex mr-5">
+          <li className=" ">
+            <FaUser className=" text-[1.5rem] font-semibold" />
+          </li>
+        </ul>
+      </div>
+    </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,15 +4,14 @@ import "../home/home.css"
 
 function Home() {
   return (
-    <div className=' fondoHome h-[60vh] w-full flex items-center text-white'>
-        <div className=' w-[30%] ml-24'>
-            <h1 className=' text-[2.9rem]'>SERVICIO DE COTIZACION ARQUITECTONICA</h1>
-            <p>Lorem ipsum dolor sit,
-                 amet consectetur adipisicing elit. Id illo similique delectus nostrum quos a unde, 
-                 ipsa cum ipsum necessitatibus totam omnis dicta modi ab cupiditate. Ratione facilis velit illum.</p>
-        </div>
-        <div className=''>
-        <button> Ver Más</button>
+    <div className=' fondoHome h-[60vh] w-full flex items-center text-white '>
+        <div className=' xl:w-[30%] md:w-[40%] sm:w-[65%] ml-20 mr-4'>
+          <div>
+            <h1 className=' mt-7 pb-3 text-2xl xl:text-3xl md:text-2xl sm:text-2xl '>SERVICIO DE COTIZACION ARQUITECTONICA</h1>
+            <p>Lorem ipsum dolor sit,amet consectetur adipisicing elit. Id illo similique delectus nostrum quos a unde,   ipsa cum ipsum necessitatibus totam omnis dicta modi ab cupiditate. Ratione facilis velit illum.
+            </p>
+            <button> Ver Más</button>
+          </div>
         </div>
     </div>
   )

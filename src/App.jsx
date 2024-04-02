73eee716bf6a47211240abf07b327fb0b74c2294
Juan Manuel Castaño <juanmanuelcastano11b@gmail.com>
2024-04-01 22:42:26 -0500

    advance

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter,useRoutes } from "react-router-dom"
 import Navbar from "./components/navbar/navbar"
-
+import Home from "./pages/home/home"
 
 function Router (){
   let router = useRoutes([
-    /* {path:"/",element:<Home/>}, */
+     {path:"/",element:<Home/>}, 
     /* {path:"/presentaciones",element:<Presentaciones/>},
     {path:"/videos",element:<Videos/>},
     {path:"/tutoriales",element:<Tutoriales/>},
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import imgNavbar from '../../assets/pexels-energepiccom-110469.jpg'
+import "../navbar/navbar.css"
+import { CiUser } from "react-icons/ci";
 
 const Navbar = () => {
   return (
-    <div>
-        holo xd, :V, :3, :c
-    </div>
+
+
+
+<div className=" fondo w-full flex  mt-5"  >
+  <div className='  '>
+   
+  <ul className=' w-full gap-10 flex '>
+    <li>Home</li>
+    <li>Nosotros</li>
+    <li>Contacto</li>
+    <li>Servicios</li>
+  </ul>
+  <ul>
+    <li><CiUser/></li>
+    <li></li>
+  </ul>
+  </div>
+</div>
+
   );
 }
 
